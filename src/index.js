0fdfec3ed03e6fa4dba2c052c2224320adba3e76
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-12 21:40:09 -0400

    add length

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ function err(message) {
   throw new Error(message);
 }
 
+function pythag(x, y) {
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+
 function Vector(x, y) {
   this.hasRequiredParams(x, y);
+  this.x = x;
+  this.y = y;
 }
 
 Vector.prototype.hasRequiredParams = function (x, y) {
@@ -14,6 +20,10 @@ Vector.prototype.hasRequiredParams = function (x, y) {
   if (y === undefined) {
     err('missing y');
   }
+};
+
+Vector.prototype.length = function () {
+  return pythag(this.x, this.y);
 };
 
 export default Vector;
--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 import { expect } from 'chai';
 import Vector from '../src';
 
+function factory(x, y) {
+  return new Vector(x, y);
+}
+
 describe('Vector', () => {
   describe('paramaters', () => {
     it('should throw if missing x', () => {
@@ -11,6 +15,15 @@ describe('Vector', () => {
     it('should throw if missing y', () => {
       const fn = () => new Vector(1);
       expect(fn).throw('y');
+    });
+  });
+
+  describe('length', () => {
+    it('should be available', () => {
+      const vector = factory(3, 9);
+      const result = vector.length();
+      const expected = Math.sqrt(3 * 3 + 9 * 9);
+      expect(result).to.equal(expected);
     });
   });
 });
